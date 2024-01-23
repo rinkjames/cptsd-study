f161a227426786fc5842ddf043d292fb4126893a
rinkjames <rinkjames@gmail.com>
2024-01-23 13:36:25 +0200

    rename src to R

---
+++ R/main.R
@@ -0,0 +1,14 @@
+library(pacman)
+p_load(here, tidyverse)
+# poLCA used for fitting lca models
+# psych used for calculating psychometrics
+
+source(here("R", '01-func.R'))
+source(here("R", '00-load.R'))
+source(here("R", '02-clean1.R'))
+source(here("R", '02-clean2.R'))
+source(here("R", '03-calc.R'))
+source(here("R", '04-alpha.R'))
+source(here("R", '05-analysis.R'))
+source(here("R", '06-figs.R'))
+ 
